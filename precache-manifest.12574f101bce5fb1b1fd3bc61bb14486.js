e0e29ffd4e8e0141f03d532a6511ed59131c313c
Игорь Горбенко <blackdoto322@gmail.com>
2020-07-20 19:42:49 +0300

    Updates

---
+++ precache-manifest.12574f101bce5fb1b1fd3bc61bb14486.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42991d7bdf35f58fc69006129b219c25",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "b2666760f55f4dee0dea",
+    "url": "/weather/static/css/main.731347a3.chunk.css"
+  },
+  {
+    "revision": "332541a45da84049fb7d",
+    "url": "/weather/static/js/2.b08254a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.b08254a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2666760f55f4dee0dea",
+    "url": "/weather/static/js/main.600e9284.chunk.js"
+  },
+  {
+    "revision": "bdd4e2bf0ce8946d3851",
+    "url": "/weather/static/js/runtime-main.51874b67.js"
+  },
+  {
+    "revision": "bd794a8913a5137343c832e85e654560",
+    "url": "/weather/static/media/cloudy.bd794a89.jpg"
+  },
+  {
+    "revision": "bcb34443f5054a0922e8fc635fac623a",
+    "url": "/weather/static/media/cloudy_sky.bcb34443.jpg"
+  },
+  {
+    "revision": "3c5f89be65509338753b8cced0766919",
+    "url": "/weather/static/media/rain.3c5f89be.jpg"
+  },
+  {
+    "revision": "23f34996937207690d7c89f641d92d88",
+    "url": "/weather/static/media/sunny.23f34996.jpg"
+  }
+]);
